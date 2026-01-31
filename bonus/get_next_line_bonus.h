ac96fd86d1f048cedb852d141994ebd34285e610
Alex <ionut.a.tanvuia@gmail.com>
2026-01-31 17:25:49 +0000

    feat: add GNL bonus files

---
+++ bonus/get_next_line_bonus.h
@@ -0,0 +1,23 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#ifndef FD_MAX
+# define FD_MAX 1024
+#endif
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int i);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+
+#endif
